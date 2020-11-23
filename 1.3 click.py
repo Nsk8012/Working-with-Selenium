27e560ac47b0cf27a0dff88923be98539659a627
Nishanth S <nishanth818@gmail.com>
2020-11-23 23:11:03 +0530

    workin on click

--- 1.3 click.py
+++ 1.3 click.py
@@ -9,17 +9,3 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://www.google.com")
-print(driver.title)
-
-search = driver.find_element_by_xpath(
-    '//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
-search.send_keys("Test")
-search.send_keys(Keys.RETURN)
-
-try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, 'rso')))
-    print(main.text)
-
-finally:
-    driver.quit()
